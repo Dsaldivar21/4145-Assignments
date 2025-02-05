0ddd8b2e73ad3290e6ab4b3cb141a3e60e80d92f
David <dsaldiva@uncc.edu>
2025-02-05 16:11:19 -0500

    added script to run on centaurus

---
+++ mergescript.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=myjob
+#SBATCH --partition=Centaurus
+#SBATCH --time=00:01:00
+
+./mergeSort 10
+./mergeSort 100
+./mergeSort 1000
+./mergeSort 10000
+./mergeSort 100000
+./mergeSort 1000000
+./mergeSort 10000000
+./mergeSort 100000000
+./mergeSort 1000000000
+
